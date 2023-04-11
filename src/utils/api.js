fde2e70be455a1eba675d2dbd39caa549581bf6c
Akhil Jacob <rysetucker@gmail.com>
2023-04-11 09:46:27 +0530

    first and last commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key": "069fa7b514mshe58186d77fafacap18d76bjsn270191d23a27",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
